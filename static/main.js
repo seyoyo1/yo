d93645120618fd7f6311998a7b0ddd1069c21139
seyoyo1 <90842433+seyoyo1@users.noreply.github.com>
2021-09-18 17:15:43 +0800

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,33 @@
+document.getElementById('link').innerHTML = '\
+    <li>\
+        <a id="btnEnter1" href="javascript:void(0)" class="gray">未满18岁</a>\
+    </li>\
+    <li>\
+        <a id="btnEnter2" href="javascript:void(0)" class="active">已满18+</a>\
+    </li>\
+    <li>\
+        <a id="btnEnter3" href="javascript:void(0)">备用网址1</a>\
+    </li>\
+    <li>\
+        <a id="btnEnter4" href="javascript:void(0)">备用网址2</a>\
+    </li>\
+    <li>\
+        <a id="btnEnter5" href="javascript:void(0)">备用网址3</a>\
+    </li>\
+';
+document.getElementById('email').innerHTML = 'seyoyo.com@gmail.com';
+document.getElementById('btnEnter1').onclick = function() {
+    window.location.href = 'http://lewaos.com';
+}
+document.getElementById('btnEnter2').onclick = function() {
+    window.location.href = 'https://seyoyo1.com';
+}
+document.getElementById('btnEnter3').onclick = function() {
+    window.location.href = 'https://seyoyo2.com';
+}
+document.getElementById('btnEnter4').onclick = function() {
+    window.location.href = 'https://seyoyo3.com';
+}
+document.getElementById('btnEnter5').onclick = function() {
+    window.location.href = 'https://seyoyo4.com';
+}
