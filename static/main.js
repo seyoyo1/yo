21bfc44e286140579f5134bdfe1a7dc62802db09
seyoyo1 <90842433+seyoyo1@users.noreply.github.com>
2022-06-16 20:15:35 +0800

    Update main.js

--- static/main.js
+++ static/main.js
@@ -20,14 +20,14 @@ document.getElementById('btnEnter1').onclick = function() {
     window.location.href = 'https://www.baidu.com';
 }
 document.getElementById('btnEnter2').onclick = function() {
-    window.location.href = 'https://113.seyoyo51.com';
+    window.location.href = 'https://117.seyoyo51.com';
 }
 document.getElementById('btnEnter3').onclick = function() {
-    window.location.href = 'https://114.seyoyo51.com';
+    window.location.href = 'https://118.seyoyo51.com';
 }
 document.getElementById('btnEnter4').onclick = function() {
-    window.location.href = 'https://115.seyoyo51.com';
+    window.location.href = 'https://119.seyoyo51.com';
 }
 document.getElementById('btnEnter5').onclick = function() {
-    window.location.href = 'https://116.seyoyo51.com';
+    window.location.href = 'https://120.seyoyo51.com';
 }
